9bf4badd74dc64b83b3171778392d3cfa4ae7237
Utsumi <kouki.utumi@gmail.com>
2021-11-27 13:29:05 +0900

    First

---
+++ Assets/Macine_U/ImgMacine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImgMacine : MonoBehaviour
+{
+    public int Macinenumber;//æ•¨‚ÌID
+    public ImgMacine (int num)
+    {
+        this.Macinenumber = num;
+    }
+}
